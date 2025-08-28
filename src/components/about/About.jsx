8be73c9ed4c73eec0bb7c38c47dc1b2c5d71762f
Tahaseen2002 <116971416+Tahaseen2002@users.noreply.github.com>
2025-08-28 18:18:21 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
             <article className='about__card'>
               <FaAward className='about__icon' />
               <h5>Experience</h5>
-              <small>1+ year Working</small>
+              <small>2+ year Working</small>
             </article>
             <article className='about__card'>
               <HiUsers className='about__icon' />
